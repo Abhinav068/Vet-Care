c523bcceaa9566a09be9fb726e04500172c99383
Abhinav068 <abhinav28897@gmail.com>
2023-06-13 21:00:12 +0530

    small changes

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,30 @@
-// const express = require("express");
-// const { connection } = require('./db');
-// const cors = require("cors");
-// require("dotenv").config();
-// const { userRouter } = require("./routes/user.router");
+const express = require("express");
+const { connection } = require('./db');
+const cors = require("cors");
+require("dotenv").config();
+const { userRouter } = require("./routes/user.router");
 
 
 
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 
 
 
 
-// app.use("/user", userRouter);
+app.use("/user", userRouter);
 
 
-// app.listen(process.env.PORT || 4900, async () => {
-//   try {
-//     // await connection;
-//     console.log("*****************Connected to DB*****************");
-//   } catch (error) {
-//     console.log("Error in DB", error);
-//   }
-// });
+app.listen(process.env.PORT || 4900, async () => {
+  try {
+    await connection;
+    console.log("*****************Connected to DB*****************");
+  } catch (error) {
+    console.log("Error in DB", error);
+  }
+});
 
 
 
