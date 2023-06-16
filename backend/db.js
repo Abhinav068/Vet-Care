c181d22c451598dad71e9af3f5d8b30fccbe3e00
Nitesh Kumar <niteshkumar240198@gmail.com>
2023-06-17 02:21:38 +0530

    day-3

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,6 @@ const mongoose=require("mongoose")
 require("dotenv").config()
 
 
-const connection=mongoose.connect(process.env.mongoURL)
+const connection=mongoose.connect(process.env.mongoURL ||"mongodb+srv://krunalgurao1:krunalgurao@vetcare.i2366lh.mongodb.net/user?retryWrites=true&w=majority")
 
 module.exports={connection}
