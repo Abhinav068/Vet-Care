456ace41284a70eca644e2d9b97abe304d92f221
Nitesh Kumar <niteshkumar240198@gmail.com>
2023-06-17 19:49:24 +0530

    day-4

--- frontend/Src/bookappointment.js
+++ frontend/Src/bookappointment.js
@@ -1,38 +1,48 @@
 
-const bookingparURL = 'http:localhost:4900'
+const bookingparURL = 'http://localhost:4900'
 
 const booktoken = localStorage.getItem('token') || null
 
-if (!booktoken) {
-    location.href = '../View/login.html'
-}
+// if (!booktoken) {
+//     location.href = '../View/login.html'
+// }
 
 
-const DoctorData =  JSON.parse(localStorage.getItem('localDR')) || []
+const DoctorData = JSON.parse(localStorage.getItem('localDR')) || []
+
+const userID = localStorage.getItem('userID') || null
 
 
 const bookdate = document.getElementById('dateField')
 
 const petcategory = document.getElementById('seletcategory')
 
-const customerName = document.getElementById('cust-Name')
 
+const timingSlots = document.getElementById('booking_timingslots')
 
-console.log("==>",DoctorData[0]._id, DoctorData[0].name)
+const DoctorID = document.getElementById('doctorID')
+
+const Dname = document.getElementById('DR_name_here')
 
 
 RenderDRinfo(DoctorData[0]._id, DoctorData[0].name)
 
 
+const imagechangediv = document.getElementById('random_images')
+
+
+const ArrayImages = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFq_GsDd9rLTifpGi809DqAQClHBDBa-KTbA&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGQLkYMmYOhpMSM574K-y_cwgdoK-f8_3QdQ&usqp=CAU", "https://www.marketplace.org/wp-content/uploads/2021/08/veterinary-care.jpg?fit=2800%2C1866", "https://cdn.sanity.io/images/0vv8moc6/dvm360/054a7b14abf5e97fd9b874b2e7f99a9e04c4b6fa-5050x3366.jpg"]
+
+
+
+setInterval(() => {
+    imagechangediv.innerHTML = `<img src="${ArrayImages[(Math.floor(Math.random() * 10)) % 4]}" alt="Image" />`
+}, 3000)
+
+
+
+
 function RenderDRinfo(did, dname) {
-
-
-    const timingSlots = document.getElementById('timingslots')
-
-    const DoctorID = document.getElementById('doctorID')
-
-    const Dname = document.getElementById('DR_name_here')
-
 
     Dname.value = dname;
 
@@ -42,9 +52,9 @@ function RenderDRinfo(did, dname) {
     // filter available slots only here
 
 
-    // contains total slots from 9-6
+    const TotalSlot = DoctorData[0].slots
 
-    const TotalSlot = []
+    // console.log("slots",TotalSlot)
 
 
     // contains avail slot for booking
@@ -52,53 +62,48 @@ function RenderDRinfo(did, dname) {
 
     const AvailSlots = []
 
-    // for () {
+    for (let slot in TotalSlot) {
 
-    //     // filter free slots and store in availslot array
+        // filter free slots and store in availslot array
 
-    // }
+        if (TotalSlot[slot].status) {
+            AvailSlots.push(TotalSlot[slot].timing)
+        }
+    }
 
 
-
-    const slots = AvailSlots.map((ele) => {
-        return getSlot(ele)
+    const slots = AvailSlots.map((ele, i) => {
+        return getSlot(ele, i + 1)
     }).join('')
 
-    timingSlots.innerHTML =  ` <option value="">Select Timing</option>${slots}`;
+    timingSlots.innerHTML = `<option value="">Select Timing</option>${slots}`;
+
 
 
 }
 
 
-function getSlot(ele) {
+function getSlot(ele,i) {
 
-    // return dynamically available slots
-
-    return `
-    
-    <option value="${ele}">${ele}</option>
-    
-    `
+    return `<option value="slot${i}">${ele}</option>`
 }
 
 
 function handleBookAppointMent() {
 
-
-    const bookingObj = {
-
-        doctorid,
-        doctorname,
-        date,
-        petcategory,
-        username,
-        slottiming
-
+    if(!Dname || !DoctorID || !bookdate.value || !petcategory.value || !timingSlots.value){
+        alert('Kindly Enter All required Details !!')
+        return
     }
 
 
-    fetch(`${bookingparURL}`, {
-        method: 'POST',
+    const bookingObj = { doctorsid:DoctorID.value, userid:userID , petcategory:petcategory.value, bookingdate:bookdate.value, appointmentdate:new Date() }
+
+    console.log("booking ==>",bookingObj)
+
+
+    fetch(`${bookingparURL}/admin/booking/${timingSlots.value}`, {
+        method: 'PATCH',
         headers: {
             'content-type': 'application/json',
             'authorization': `Bearer ${booktoken}`
